37c4f200bac716ddc32bb4c58cb7d4527275ea15
young <young@young.local>
2020-09-25 14:44:41 +0800

    .

---
+++ twitter.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python
+#Copyright wqyang@bu.edu
+
+from TwitterAPI import TwitterAPI
+import config.twitterAPIConfig
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
