36aba08629f5207be8611932d718676d786d4dea
young <young@young.local>
2020-09-30 14:35:58 +0800

    xxxx

--- practice/twitter.py
+++ practice/twitter.py
@@ -1,7 +1,11 @@
 #!usr/bin/env python
 #Copyright wqyang@bu.edu
 
-from TwitterAPI import TwitterAPI
-import config.twitterAPIConfig
+import tweepy
+from config.twitterAPIConfig import *
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
