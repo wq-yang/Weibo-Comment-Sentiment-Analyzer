29609e2762e45917373c89a117518eaa752ed038
young <young@young.local>
2020-11-08 23:58:53 +0800

    use gpg to encrypt

--- .github/encrypted_config/decrypt_secret.sh
+++ .github/encrypted_config/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/Google_API_Key.json encrypted_config/Google_API_Key.json.gpg
+--output $HOME/secrets/Google_API_Key.json Google_API_Key.json.gpg
